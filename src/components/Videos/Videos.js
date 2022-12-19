a7849aeb333aabd4ea66f406a11e3f3d7162a0a6
Muhammad Aslam <kamuhammadaslam@gmail.com>
2022-12-19 17:51:57 +0530

    finaly the prg is finished

--- src/components/Videos/Videos.js
+++ src/components/Videos/Videos.js
@@ -27,34 +27,7 @@ function Videos() {
         <video className='post' src="./videos/vid19.mp4" controls ></video>
         <video className='post' src="./videos/vid20.mp4" controls ></video>
         <video className='post' src="./videos/vid21.mp4" controls ></video>
-        <video className='post' src="./videos/vid22.mp4" controls ></video>
-        <video className='post' src="./videos/vid23.mp4" controls ></video>
-        <video className='post' src="./videos/vid24.mp4" controls ></video>
-        <video className='post' src="./videos/vid25.mp4" controls ></video>
-        <video className='post' src="./videos/vid26.mp4" controls ></video>
-        <video className='post' src="./videos/vid27.mp4" controls ></video>
-        <video className='post' src="./videos/vid28.mp4" controls ></video>
-        <video className='post' src="./videos/vid29.mp4" controls ></video>
-        <video className='post' src="./videos/vid30.mp4" controls ></video>
-        <video className='post' src="./videos/vid31.mp4" controls ></video>
-        <video className='post' src="./videos/vid32.mp4" controls ></video>
-        <video className='post' src="./videos/vid33.mp4" controls ></video>
-        <video className='post' src="./videos/vid34.mp4" controls ></video>
-        <video className='post' src="./videos/vid35.mp4" controls ></video>
-        <video className='post' src="./videos/vid36.mp4" controls ></video>
-        <video className='post' src="./videos/vid37.mp4" controls ></video>
-        <video className='post' src="./videos/vid38.mp4" controls ></video>
-        <video className='post' src="./videos/vid39.mp4" controls ></video>
-        <video className='post' src="./videos/vid40.mp4" controls ></video>
-        <video className='post' src="./videos/vid41.mp4" controls ></video>
-        <video className='post' src="./videos/vid42.mp4" controls ></video>
-        <video className='post' src="./videos/vid43.mp4" controls ></video>
-        <video className='post' src="./videos/vid44.mp4" controls ></video>
-        <video className='post' src="./videos/vid45.mp4" controls ></video>
-        <video className='post' src="./videos/vid46.mp4" controls ></video>
-        <video className='post' src="./videos/vid47.mp4" controls ></video>
-        <video className='post' src="./videos/vid48.mp4" controls ></video>
-        <video className='post' src="./videos/vid49.mp4" controls ></video>
+        
     </div>
   )
 }
